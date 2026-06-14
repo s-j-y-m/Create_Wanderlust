d03fac46a02c5e98a8e27eb92d9acadbd3211270
s-j-y-m <917966079@qq.com>
2026-06-14 12:11:59 +0800

    Release v0.3.0-Alpha1

---
+++ kubejs/server_scripts/pioneer.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.shapeless('minecraft:crying_obsidian', [
+        'minecraft:obsidian',
+        'farmersdelight:onion'
+    ]).id('kubejs:crying_obsidian_from_obsidian_and_onion')
+})
--- kubejs/startup_scripts/pioneer.js
+++ kubejs/startup_scripts/pioneer.js
@@ -1,3 +1,21 @@
-StartupEvents.registry('item', event => { 
-    event.create("pioneer:test","basic")
+const AttributeModifierOperation = Java.loadClass('net.minecraft.world.entity.ai.attributes.AttributeModifier$Operation')
+const MECHANICAL_HEART_SLOT = 'mechanical_heart'
+
+function mechanicalHeartCurio(speed, modifierName) {
+    return CuriosJSCapabilityBuilder.create()
+        .canEquip((slotContext, stack) => String(slotContext.identifier()) == MECHANICAL_HEART_SLOT)
+        .addAttribute(
+            'minecraft:generic.movement_speed',
+            modifierName,
+            speed,
+            AttributeModifierOperation.ADDITION
+        )
+}
+
+StartupEvents.registry('item', event => {
+    event.create('pioneer:test', 'basic')
+
+    event.create('pioneer:wooden_mechanical_heart', 'basic')
+        .displayName('木制机械之心')
+        .attachCuriosCapability(mechanicalHeartCurio(0.25, 'wooden_mechanical_heart_movement_speed'))
 })
